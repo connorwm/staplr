4535b98a227555ddf52ca5d98033b1cd92d5fb29
connorwm <murphy18241@gmail.com>
2015-01-07 03:28:09 -0600

    Added accessor to parent (Commuication)

--- net/staplr/common/Communicator.java
+++ net/staplr/common/Communicator.java
@@ -16,6 +16,7 @@ import net.staplr.common.Settings;
 import net.staplr.logging.Log;
 import net.staplr.logging.Entry;
 import net.staplr.logging.LogHandle;
+import net.staplr.master.Communication;
 import net.staplr.master.Feeds;
 import net.staplr.master.Master;
 import FFW.Network.*;
@@ -25,6 +26,7 @@ import FFW.Network.*;
  */
 public class Communicator implements Runnable
 {
+	private Communication c_communication;
 	private DefaultSocketConnection sc_listener;
 	private Settings s_settings;
 	private boolean b_run;
@@ -52,8 +54,9 @@ public class Communicator implements Runnable
 	 * @param f_feeds - Object of feeds this master is responsible for
 	 * @author murphyc1
 	 */
-	public Communicator(Settings s_settings, int i_port, Type t_type, Log l_main, Feeds f_feeds)
+	public Communicator(Communication c_communication, Settings s_settings, int i_port, Type t_type, Log l_main, Feeds f_feeds)
 	{
+		this.c_communication = c_communication;
 		this.s_settings = s_settings;
 		sc_listener = new DefaultSocketConnection(i_port);
 		this.t_type = t_type;		
@@ -120,7 +123,7 @@ public class Communicator implements Runnable
 		}
 		synchronized (arr_worker) 
 		{
-			arr_worker.add(new Worker(sc_newClient, s_settings, l_main, b_isJoining, t_type, f_feeds, this));
+			arr_worker.add(new Worker(sc_newClient, s_settings, l_main, b_isJoining, t_type, f_feeds, c_communication));
 			es_workerPool.submit(arr_worker.get(arr_worker.size()-1));
 			
 			w_new = arr_worker.get(arr_worker.size()-1);
@@ -167,7 +170,7 @@ public class Communicator implements Runnable
 		return null;
 	}
 	
-	/**Returns a mapping of the addresses and ports of the connected masters
+	/**Returns a mapping of the addresses and ports of the currently connected masters
 	 * @return Map of the addresses to the ports
 	 * @author murphyc1
 	 */
@@ -184,98 +187,6 @@ public class Communicator implements Runnable
 		}
 		
 		return map_masterAddresses;
-	}
-	
-	/**Redistributes feeds from a master when said master goes down
-	 * @param Address of master in the form of XXX.XXX.XXX:PORT
-	 * @author murphyc1
-	 */
-	public synchronized void redistributeFeeds(String str_address)
-	{
-		lh_communication.write("Redistributing feeds from "+str_address);
-		
-		ArrayList<String> arr_lostFeeds = f_feeds.getAssignments().get(str_address);
-		String str_feeds = "";
-		
-		for(String str_feed : arr_lostFeeds)
-		{
-			str_feeds += str_feed+"\r\n";
-		}
-		
-		lh_communication.write("Removing remnants of connection...");
-		
-		// Remove remnants of the connection
-		Object o_toRemove = null;
-		
-		for(Worker w_worker : arr_worker) 
-		{
-			if(w_worker.getClientAddress() == str_address) 
-			{
-				o_toRemove = w_worker;
-				break;
-			}
-		}
-		
-		arr_worker.remove(o_toRemove);
-		o_toRemove = null;
-		
-		for(DefaultSocketConnection sc_client : arr_client) 
-		{
-			if(sc_client.getAddress() == str_address) 
-			{
-				o_toRemove = sc_client;
-				break;
-			}
-		}
-		
-		arr_worker.remove(o_toRemove);
-		
-		lh_communication.write("Completed removal");
-		lh_communication.write("Lost feeds from "+str_address+": \r\n"+str_feeds);
-		
-		// Determine course of action depending on how many other masters there are
-		if(arr_worker.size() == 0)
-		{
-			lh_communication.write("Assigning lost feeds to self; no other masters active");
-			
-			// Put together a feed object with as little known variables as possible (2)
-			ArrayList<Feed> arr_lostFeedsPartial = new ArrayList<Feed>();
-			
-			for(int i_feedIndex = 0; i_feedIndex < arr_lostFeeds.size(); i_feedIndex++)
-			{
-				Feed f_feed = new Feed();
-				f_feed.set(Feed.Properties.collection, arr_lostFeeds.get(i_feedIndex).substring(0, arr_lostFeeds.get(i_feedIndex).indexOf(":")));
-				f_feed.set(Feed.Properties.name, arr_lostFeeds.get(i_feedIndex).substring(arr_lostFeeds.get(i_feedIndex).indexOf(":")+1));
-				
-				arr_lostFeedsPartial.add(f_feed);
-			}
-			
-			f_feeds.arr_feed.addAll(arr_lostFeedsPartial);
-			
-			lh_communication.write("Now responsible for "+f_feeds.arr_feed.size());
-		}
-		else
-		{
-			lh_communication.write("Contacting other masters to distribute lost feeds");
-			
-			/*
-			 * To reorganize the lost feeds:
-			 * Each master will send a random number to other masters
-			 * The master with the highest number distributes the feeds
-			 * 
-			 * Since this cannot be accomplished on this thread we will save
-			 * the lost feeds in the Feeds object
-			 */
-			
-			f_feeds.setLostFeeds(arr_lostFeeds);
-			
-			Message msg_redistributeNumber = new Message(Message.Type.Request, Value.RedistributeNumber);
-			msg_redistributeNumber.addItem("number", String.valueOf((new Random()).nextInt(101)));
-			
-			broadcast(msg_redistributeNumber);
-		}
-		
-		lh_communication.write("Feed distribution now resolved");
 	}
 	
 	/**Broadcasts a message to all connected masters
@@ -309,5 +220,10 @@ public class Communicator implements Runnable
 	public int getConnectionCount()
 	{
 		return arr_client.size();
+	}
+	
+	public Communication getParent()
+	{
+		return c_communication;
 	}
 }
