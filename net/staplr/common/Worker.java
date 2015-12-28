cdb2d8030148f38176a1bb45e4f34f1621c36170
Connor <murphy18241@gmail.com>
2015-12-28 16:59:19 -0600

    Cleaned up Worker class and improved error handling as well as documented code. Also enabled the disconnect of a client to be expected so as to not handle it as always an error such as when the client requests it.

--- net/staplr/common/Worker.java
+++ net/staplr/common/Worker.java
@@ -2,14 +2,14 @@ package net.staplr.common;
 
 import java.util.ArrayList;
 
+import FFW.Network.DefaultSocketConnection;
+
 import net.staplr.common.Settings;
 import net.staplr.logging.Entry;
 import net.staplr.logging.Log;
 import net.staplr.logging.LogHandle;
 import net.staplr.master.Communication;
 import net.staplr.master.Feeds;
-import net.staplr.master.Master;
-import FFW.Network.DefaultSocketConnection;
 import net.staplr.common.message.Message;
 import net.staplr.common.message.MessageEnsurer;
 import net.staplr.common.message.Message.Type;
@@ -19,7 +19,6 @@ import net.staplr.common.Communicator;
 public class Worker implements Runnable
 {
 	private DefaultSocketConnection sc_client;
-	private Master master;
 	private MessageEnsurer me_ensurer;
 	public net.staplr.common.message.MessageExecutor mx_executor;
 	private Thread t_ensurer;
@@ -29,8 +28,9 @@ public class Worker implements Runnable
 	private ArrayList<Message> msg_outbox;
 	
 	private Communicator.Type t_type;
-	private boolean b_run;
 	private LogHandle lh_worker;
+	private boolean b_run;
+	private boolean b_disconnecting;
 	
 	public Worker(DefaultSocketConnection sc_client, Settings s_settings, Log l_main, 
 			boolean b_isJoining, Communicator.Type t_type, Feeds f_feeds, Communication c_communication)
@@ -41,24 +41,28 @@ public class Worker implements Runnable
 		this.c_communication = c_communication; 
 		
 		b_run = true;
+		b_disconnecting = false; // This variable is set when the client requests a Disconnect
+								 // MessageExecutor will set this variable
 		
 		lh_worker = new LogHandle("wrkr", l_main);
 		lh_worker.write("Working for client at "+sc_client.getAddress());
 		
+		// Setup MessageExecutor depending on type of Worker
 		switch(t_type)
 		{
 		case Service:
-			mx_executor = new net.staplr.service.MessageExecutor(sc_client, lh_worker, me_ensurer, msg_inbox, msg_outbox, s_settings, f_feeds);
+			mx_executor = new net.staplr.service.MessageExecutor(sc_client, lh_worker, me_ensurer, msg_inbox, msg_outbox, s_settings, f_feeds, this);
 			break;
 		case Master:
-			mx_executor = new net.staplr.master.MessageExecutor(sc_client, lh_worker, me_ensurer, msg_inbox, msg_outbox, s_settings, c_communication.getMasterCommunicator(), f_feeds);
+			mx_executor = new net.staplr.master.MessageExecutor(sc_client, lh_worker, me_ensurer, msg_inbox, msg_outbox, s_settings, c_communication.getMasterCommunicator(), f_feeds, this);
 			break;
 		}
 		
 		this.t_type = t_type;
 		
-		me_ensurer = new MessageEnsurer(sc_client, mx_executor, msg_inbox, msg_inbox, s_settings, lh_worker);
 		
+		// Initiate and start a MessageEnsurer for this Worker
+		me_ensurer = new MessageEnsurer(sc_client, mx_executor, msg_inbox, msg_inbox, s_settings, lh_worker);
 	
 		t_ensurer = new Thread(me_ensurer);
 		t_ensurer.start();
@@ -68,19 +72,22 @@ public class Worker implements Runnable
 	{
 		Message msg_received = null;
 		String str_received = new String();
-		// ----------------------------------------------------
-		// Main Loop
-		// ----------------------------------------------------
 
 		while(b_run)
 		{			
-			// TODO Fix ambiguation with being connected vs being closed in FFW library
+			// TODO Fix ambiguation with being connected versus being closed in FFW library
 			if(!sc_client.isClosed())
 			{
 				try
 				{
 					str_received = sc_client.receive();
-					
+				}
+				catch (Exception excep_receive)
+				{
+					lh_worker.write(Entry.Type.Error, "Could not receive message:\r\n"+excep_receive.toString());					
+				}
+				finally
+				{
 					if(str_received != null)
 					{
 						lh_worker.write("Received: "+str_received);
@@ -91,27 +98,27 @@ public class Worker implements Runnable
 						lh_worker.write(Entry.Type.Error, "Received null from client; disconnecting...");
 						sc_client.close();
 					}
-				}
-				catch (Exception e)
-				{
-					lh_worker.write(Entry.Type.Error, "Could not receive message:\r\n"+e.toString());					
-				}
-				finally
-				{
-					msg_inbox.add(msg_received);
 					
-					if(msg_received.isValid())
+					// If there was a message received and it was parsed then add it to the inbox
+					if(msg_received != null && str_received != null)
 					{
-						mx_executor.execute(msg_received);
-					}
-					else 
-					{
-						lh_worker.write(Entry.Type.Error, "Received invalid message:\r\n"+msg_received.toString());
+						msg_inbox.add(msg_received);
 						
-						Message msg_invalidMessage = new Message(Type.Response, Value.Invalid);
-						msg_invalidMessage.addItem("message", msg_received.toString());
+						// If the message is valid then we may execute it
+						if(msg_received.isValid())
+						{
+							mx_executor.execute(msg_received);
+						}
+						else
+						{							
+							lh_worker.write(Entry.Type.Error, "Received invalid message:\r\n"+msg_received.toString());
 						
-						mx_executor.send(msg_invalidMessage);
+							// If not then send an invalid message response
+							Message msg_invalidMessage = new Message(Type.Response, Value.Invalid);
+							msg_invalidMessage.addItem("message", msg_received.toString());
+							
+							mx_executor.send(msg_invalidMessage);
+						}
 					}
 				}
 			}
@@ -120,10 +127,15 @@ public class Worker implements Runnable
 				lh_worker.write("Client no longer connected; shutting down...");
 				b_run = false;
 				
-				if(t_type == Communicator.Type.Master)
-				{					
-					c_communication.verifyDownMaster(getClientAddress());		
-				}	
+				// If a disconnect was unexpected (not requested by client) then handle accordingly
+				if(!b_disconnecting)
+				{
+					// If this Worker was connected to another master then we need to verify it is down from other masters
+					if(t_type == Communicator.Type.Master)
+					{					
+						c_communication.verifyDownMaster(getClientAddress());		
+					}
+				}
 			}
 		}
 	}
@@ -131,5 +143,10 @@ public class Worker implements Runnable
 	public String getClientAddress()
 	{
 		return sc_client.getAddress();
+	}
+	
+	public void setExpectDisconnect()
+	{
+		b_disconnecting = true;
 	}
 }
