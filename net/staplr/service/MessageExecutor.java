caab74373fc1cd4af00dab8f96c0f27ade1d4aa0
Connor <murphy18241@gmail.com>
2015-12-28 12:27:22 -0600

    Cleaned up Service MessageExecutor with documentation and improved error handling.

--- net/staplr/service/MessageExecutor.java
+++ net/staplr/service/MessageExecutor.java
@@ -1,15 +1,12 @@
 package net.staplr.service;
 
 import java.awt.TextArea;
-import java.awt.TextField;
 import java.io.BufferedReader;
-import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Iterator;
 
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -29,7 +26,7 @@ import net.staplr.common.message.MessageEnsurer;
 import net.staplr.common.message.Message.Type;
 import net.staplr.common.message.Message.Value;
 import net.staplr.common.TextFieldLogger;
-import net.staplr.logging.Log;
+import net.staplr.logging.Entry;
 import net.staplr.logging.LogHandle;
 import net.staplr.master.Feeds;
 import FFW.Network.DefaultSocketConnection;
@@ -87,12 +84,13 @@ public class MessageExecutor extends net.staplr.common.message.MessageExecutor
 				msg_settings.addItem("databaseAuth", json_databaseAuth);
 				
 				msg_settings.addItem("masterKey", String.valueOf(s_settings.get(Setting.masterKey)));
-				msg_settings.addItem("masterCommunicationPort", String.valueOf(s_settings.get(Setting.masterCommunicationPort)));
+				msg_settings.addItem("masterCommunicationPort", String.valueOf(s_settings.get(Setting.masterPort)));
 				msg_settings.addItem("servicePort", String.valueOf(s_settings.get(Setting.servicePort)));
 				
 				super.send(msg_settings);
 				msg_message.setRespondedTo();
 			}
+			
 			//////////////////////////////////////////////////////////////////////////////////////////////////
 			//	UPDATE SETTINGS
 			//////////////////////////////////////////////////////////////////////////////////////////////////
@@ -103,6 +101,7 @@ public class MessageExecutor extends net.staplr.common.message.MessageExecutor
 				DatabaseAuth auth_settingsDB = new DatabaseAuth();
 				JSONObject json_auth = (JSONObject)msg_message.get("databaseAuth");
 				
+				// Grab database authorization properties from JSON node in message and put those properties into our configuration
 				for(int i_propertyIndex = 0; i_propertyIndex < DatabaseAuth.Properties.values().length; i_propertyIndex++)
 				{
 					if(json_auth.containsKey(Properties.values()[i_propertyIndex].toString()))
@@ -111,8 +110,10 @@ public class MessageExecutor extends net.staplr.common.message.MessageExecutor
 					}
 				}
 				
+				// Set the Settings object's properties to the properties specified by the message
 				s_settings.setSettingsDBAuth(auth_settingsDB);
 				
+				// Now set the other properties in Settings to those specified by the message
 				for(int i_settingIndex = 0; i_settingIndex < Settings.Setting.values().length; i_settingIndex++)
 				{
 					if(msg_message.get(Settings.Setting.values()[i_settingIndex].toString()) != null)
@@ -123,6 +124,7 @@ public class MessageExecutor extends net.staplr.common.message.MessageExecutor
 				
 				lh_worker.write("\tComplete");
 			}
+			
 			//////////////////////////////////////////////////////////////////////////////////////////////////
 			//	LOGS
 			//////////////////////////////////////////////////////////////////////////////////////////////////
@@ -130,51 +132,75 @@ public class MessageExecutor extends net.staplr.common.message.MessageExecutor
 			{
 				Message msg_logContents = new Message(Type.Response, Value.Logs);
 				InputStream is_log = null;
-				BufferedReader br = null;
-				DateTime dt_now = new DateTime(DateTimeZone.getDefault());
+				BufferedReader br_log = null;
 				String str_logLine = null;
-				String str_logFile = "/var/www/master/staplr-" + dt_now.toString("M.d.y") + ".log"; // TODO Hard coded value of directory
+				String str_logFile = lh_worker.getLog().getLogFilePath();
 				String str_logContents = "";
 				
+				// Open log file and load the contents into a stream
 				if(str_logFile != null)
 				{
-					try {
-						is_log = this.getClass().getResourceAsStream(str_logFile);
-					} catch (Exception e) {
-						lh_worker.write("ERROR: Cannot open master log file: "+e.toString());
-						str_logContents = "-- Error when Opening Log --";
+					// Try and open the log
+					try 
+					{
+						is_log = new FileInputStream(str_logFile);
 					} 
-					finally {
-						try {
-							br = new BufferedReader(new InputStreamReader(is_log));
-							
-							while ((str_logLine = br.readLine()) != null)
+					catch (Exception excep_openLog) 
+					{
+						lh_worker.write(Entry.Type.Error, "Cannot open master log file: "+excep_openLog.toString());
+						str_logContents = "-- Error when Opening Log --";
+					}
+					finally 
+					{
+						// If log file was opened, read it line by line into the string
+						if(is_log != null)
+						{
+							try 
 							{
-								str_logContents += str_logLine;
+								br_log = new BufferedReader(new InputStreamReader(is_log));
+								
+								while ((str_logLine = br_log.readLine()) != null)
+								{
+									str_logContents += str_logLine +"\r\n";
+								}
+							}
+							catch (Exception excep_readLog)
+							{
+								lh_worker.write(Entry.Type.Error, "Problem while reading log file: "+excep_readLog.toString());
+								
+								str_logContents += "-- Error Encountered while Reading Log --";
 							}
 							
-							br.close();
-							is_log.close();
-						}
-						catch (Exception e)
-						{
-							lh_worker.write("ERROR: During reading log file: "+e.toString());
+							// Close log buffered reader
+							try
+							{
+								br_log.close();
+							} 
+							catch (Exception excep_readerClose) 
+							{
+								lh_worker.write(Entry.Type.Error, "Failed to close log buffered reader: "+excep_readerClose.toString());
+							}
 							
-							str_logContents += "-- Error Encountered While Reading --";
-						
-							try{
-								br.close();
-							} catch (Exception closeException) {}
-							try{
+							// Close log file input stream
+							try
+							{
 								is_log.close();
-							} catch (Exception closeException) {}
+							} 
+							catch (Exception excep_streamClose) 
+							{
+								lh_worker.write(Entry.Type.Error, "Failed to close log input stream: "+excep_streamClose.toString());
+							}
 						}
 					}
 				}
 				
+				// Place contents into response message
+				// In the event that an error was encountered during reading the contents will contain that error
 				msg_logContents.addItem("contents", str_logContents);
+				
 				super.send(msg_logContents);
 			}
+			
 			//////////////////////////////////////////////////////////////////////////////////////////////////
 			//	GARBAGE COLLECTION
 			//////////////////////////////////////////////////////////////////////////////////////////////////
@@ -184,6 +210,7 @@ public class MessageExecutor extends net.staplr.common.message.MessageExecutor
 				System.gc();
 				lh_worker.write("\tComplete");
 			}
+			
 			//////////////////////////////////////////////////////////////////////////////////////////////////
 			//	FEEDS
 			//////////////////////////////////////////////////////////////////////////////////////////////////
@@ -194,16 +221,19 @@ public class MessageExecutor extends net.staplr.common.message.MessageExecutor
 				
 				lh_worker.write("Received feeds request");
 				
+				// Put all feeds this master is responsible for and place them into an array
 				for(String str_feed : f_feeds.getAssignments().get("127.0.0.1"))
 				{
 					json_feeds.add(str_feed);
 				}
 				
+				// Add array of feed names to message to send back to client
 				msg_feedList.addItem("feeds", json_feeds);
 				lh_worker.write("Sending back list of "+json_feeds.size()+" feeds");
 				
 				super.send(msg_feedList);
 			}
+			
 			//////////////////////////////////////////////////////////////////////////////////////////////////
 			//	DISCONNECT
 			//////////////////////////////////////////////////////////////////////////////////////////////////
@@ -214,10 +244,10 @@ public class MessageExecutor extends net.staplr.common.message.MessageExecutor
 				try
 				{
 					sc_client.close();
-				} 
-				catch(Exception e)
+				}
+				catch(Exception excep_disconnect)
 				{
-					lh_worker.write("Failed to disconnect");
+					lh_worker.write(Entry.Type.Error, "Exception occurred while disconnecting: "+excep_disconnect.toString());
 				}
 				
 				if(sc_client.isClosed())
@@ -257,10 +287,9 @@ public class MessageExecutor extends net.staplr.common.message.MessageExecutor
 				
 					tf_logger.log("Settings parsed successfully", TextFieldLogger.SuccessStyle);
 				}
-				catch (Exception e)
+				catch (Exception excep_parseSettings)
 				{
-					tf_logger.log("Encountered error while parsing settings: "+e.toString(), TextFieldLogger.ErrorStyle);
-					e.printStackTrace();
+					tf_logger.log("Encountered error while parsing received settings: "+excep_parseSettings.toString(), TextFieldLogger.ErrorStyle);
 				}
 			}
 			//////////////////////////////////////////////////////////////////////////////////////////////////
@@ -270,17 +299,27 @@ public class MessageExecutor extends net.staplr.common.message.MessageExecutor
 			{
 				lh_worker.write("Received logs response");
 	
-				String str_logContents = String.valueOf(msg_message.get("contents"));
+				String str_logContents = null;
 				TextArea ta_log = (TextArea) fi_main.get("log");
 				
 				if(ta_log != null)
 				{
-					lh_worker.write("Updating logs pane...");
-					ta_log.setText(str_logContents);
-					ta_log.setCaretPosition(str_logContents.length()-1);
-					lh_worker.write("\tComplete");
+					if(msg_message.get("contents") != null)
+					{
+						str_logContents = (String) msg_message.get("contents");
+						
+						lh_worker.write("Updating log contents...");
+						ta_log.setText(str_logContents);
+						ta_log.setCaretPosition(str_logContents.length() - 1);
+						lh_worker.write("\tComplete");
+					}
+					else
+					{
+						lh_worker.write(Entry.Type.Error, "Logs response does not have contents");
+						super.respondInvalid(msg_message);
+					}
 					
-				} else lh_worker.write("Logs response is empty");
+				} else lh_worker.write(Entry.Type.Error, "Could not get log TextArea");
 			}
 			//////////////////////////////////////////////////////////////////////////////////////////////////
 			//	FEEDS
@@ -297,10 +336,9 @@ public class MessageExecutor extends net.staplr.common.message.MessageExecutor
 				{
 					json_feeds = (JSONArray)msg_message.get("feeds");
 				}
-				catch(Exception e)
+				catch(Exception excep_getFeeds)
 				{
-					lh_worker.write("Failed to convert to JSON array object");
-					e.printStackTrace();
+					lh_worker.write(Entry.Type.Error, "Failed to get feeds array from message: "+excep_getFeeds);
 				}
 				
 				if(json_feeds  != null)
@@ -318,11 +356,10 @@ public class MessageExecutor extends net.staplr.common.message.MessageExecutor
 						{
 							arr_feeds[i_feed] = (String)arr_rawFeeds[i_feed];
 						}
-					} 
-					catch (Exception e)
+					}
+					catch (Exception excep_parseFeeds)
 					{
-						lh_worker.write("Failure parsing received feed list: possible erraneous message");
-						e.printStackTrace();
+						lh_worker.write(Entry.Type.Error, "Exception occurred while parsing feeds list: "+excep_parseFeeds.toString());
 					}
 					
 					if(arr_feeds != null)
@@ -342,10 +379,9 @@ public class MessageExecutor extends net.staplr.common.message.MessageExecutor
 
 									b_parsed = true;
 								} 
-								catch (Exception e)
+								catch (Exception excep_parseFeedNames)
 								{
-									lh_worker.write("Failure parsing received feed list: possible misformatted feed: '"+str_feed+"'");
-									e.printStackTrace();
+									lh_worker.write("Failure parsing received feed list: possible misformatted feed: '"+str_feed+"': "+excep_parseFeedNames.toString());
 								}
 
 								if(b_parsed) str_feeds += (str_collection+"\t\t\t"+str_feed+"\n");
@@ -356,11 +392,7 @@ public class MessageExecutor extends net.staplr.common.message.MessageExecutor
 					lh_worker.write("Finished parsing");
 				
 					ta_feeds.setText(str_feeds);
-					lh_worker.write("Set feeds");
-				}
-				else
-				{
-					lh_worker.write("Feeds was null after retrieval from message");
+					lh_worker.write("Updated feeds TextArea with received feed names");
 				}
 			}
 		}
