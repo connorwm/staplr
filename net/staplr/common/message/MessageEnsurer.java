746f8938e08c65eb21695d3ff97418eb6c53374a
Connor <murphy18241@gmail.com>
2015-12-28 17:04:00 -0600

    Improved error handling and added documentation to MessageEnsurer.

--- net/staplr/common/message/MessageEnsurer.java
+++ net/staplr/common/message/MessageEnsurer.java
@@ -22,6 +22,7 @@ public class MessageEnsurer implements Runnable
 	private MessageExecutor mx_executor;
 	private ArrayList<Message> msg_inbox;
 	private ArrayList<Message> msg_outbox;
+	private ArrayList<Message> msg_removeQueue;
 	private Settings s_settings;
 	
 	public MessageEnsurer(DefaultSocketConnection sc_client, MessageExecutor mx_executor, ArrayList<Message> msg_inbox, ArrayList<Message> msg_outbox, Settings s_settings, LogHandle lh_worker)
@@ -33,87 +34,89 @@ public class MessageEnsurer implements Runnable
 		this.s_settings = s_settings;
 		this.lh_worker = lh_worker;
 		
+		msg_removeQueue = new ArrayList<Message>();
 		b_run = true;
 	}
 	
 	public void run()
-	{
-		ArrayList<Message> msg_removeQueue = new ArrayList<Message>();
-		
+	{		
 		while(b_run)
 		{
-			try{
-				// ------------------------------------------------------------
-				// Inbox
-				// ------------------------------------------------------------
-				//System.out.println("Inbox size: "+msg_inbox.size());
-				for(int i_messageIndex = 0; i_messageIndex < msg_inbox.size(); i_messageIndex++)
+			// ------------------------------------------------------------
+			// Inbox
+			// ------------------------------------------------------------
+			//System.out.println("Inbox size: "+msg_inbox.size());
+			for(int i_messageIndex = 0; i_messageIndex < msg_inbox.size(); i_messageIndex++)
+			{
+				// For each inbox message check and see if it has been fully handled
+				// If so, we may remove it
+				Message msg_in = msg_inbox.get(i_messageIndex);
+
+				if(msg_in != null)
 				{
-					Message msg_in = msg_inbox.get(i_messageIndex);
-					
-					if(msg_in != null)
+					// Check if it requires response
+					if(msg_in.requiresResponse())
 					{
-						if(msg_in.requiresResponse())
+						// If so, check if it has been responded it
+						if(msg_in.respondedTo())
 						{
-							if(msg_in.respondedTo())
-							{
-								lh_worker.write("Removing responded to received message:\r\n"+msg_in);
-								msg_removeQueue.add(msg_in);
-							}
-						} 
-						else 
+							// If it has been responded to then there is no longer a need for keeping it
+							lh_worker.write("Removing responded to received message:\r\n"+msg_in);
+							msg_removeQueue.add(msg_in);
+						}
+					} 
+					else 
+					{
+						// If it does not require a response check and see how long it has been here
+						DateTime dt_timeSent = msg_in.getTimeReceived();
+
+						// If it has been sitting around for over a minute then there is no need to keep it
+						if(dt_timeSent.plusMinutes(1).isBeforeNow())
 						{
-							DateTime dt_timeSent = msg_in.getTimeReceived();
-							
-							if(dt_timeSent.plusMinutes(1).isBeforeNow())
-							{
-								lh_worker.write("Removing old received message:\r\n"+msg_in);
-								msg_removeQueue.add(msg_in);
-							}
+							lh_worker.write("Removing old received message:\r\n"+msg_in);
+							msg_removeQueue.add(msg_in);
 						}
 					}
 				}
-				
-				if(msg_removeQueue.size() > 0)	
+			}
+
+			// Items to be removed are added to the remove queue
+			// This ensures that the looping is not interrupted by removing a message while parsing all messages in the inbox array
+			// If the remove queue has items we may now remove them
+			if(msg_removeQueue.size() > 0)	
+			{
+				msg_inbox.removeAll(msg_removeQueue);
+				msg_removeQueue.clear();
+			}
+
+			// ------------------------------------------------------------
+			// Outbox
+			// ------------------------------------------------------------
+			for(int i_messageIndex = 0; i_messageIndex < msg_outbox.size(); i_messageIndex++)
+			{
+				Message msg_out = msg_outbox.get(i_messageIndex);
+
+				if(msg_out.getTimeSent() != null) // TODO incessant error at this line when master disconnects from client after Request:Disconnect message
 				{
-					msg_inbox.removeAll(msg_removeQueue);
-					msg_removeQueue.clear();
-				}
-				
-				// ------------------------------------------------------------
-				// Outbox
-				// ------------------------------------------------------------
-				for(int i_messageIndex = 0; i_messageIndex < msg_outbox.size(); i_messageIndex++)
-				{
-					Message msg_out = msg_outbox.get(i_messageIndex);
-					
-					if(msg_out.getTimeSent() != null)
+					if(msg_out.isDelivered())
 					{
-						if(msg_out.isDelivered())
+						if(msg_out.requiresResponse())
 						{
-							if(msg_out.requiresResponse())
+							if(!msg_out.hasReceivedResponse())
 							{
-								if(!msg_out.hasReceivedResponse())
+								DateTime dt_timeSent = msg_out.getTimeSent();
+
+								if(dt_timeSent.plusMinutes(1).isBeforeNow())
 								{
-									DateTime dt_timeSent = msg_out.getTimeSent();
-									
-									if(dt_timeSent.plusMinutes(1).isBeforeNow())
-									{
-										lh_worker.write(Entry.Type.Error, "Message missing required response:\r\n"+msg_out);
-										
-										Message msg_responseRequest = new Message(Type.Request, Value.ResponseRequest);
-										msg_responseRequest.addItem("message", msg_out.toString());
-										
-										mx_executor.send(msg_responseRequest);
-									}
-								} 
-								else
-								{
-									lh_worker.write("Removing old sent message:\r\n"+msg_out);
-									msg_removeQueue.add(msg_out);
+									lh_worker.write(Entry.Type.Error, "Message missing required response:\r\n"+msg_out);
+
+									Message msg_responseRequest = new Message(Type.Request, Value.ResponseRequest);
+									msg_responseRequest.addItem("message", msg_out.toString());
+
+									mx_executor.send(msg_responseRequest);
 								}
 							} 
-							else 
+							else
 							{
 								lh_worker.write("Removing old sent message:\r\n"+msg_out);
 								msg_removeQueue.add(msg_out);
@@ -121,36 +124,46 @@ public class MessageEnsurer implements Runnable
 						} 
 						else 
 						{
-							DateTime dt_timeSent = msg_out.getTimeSent();
-							
-							if(dt_timeSent.plusMinutes(1).isBeforeNow())
-							{
-								lh_worker.write("Message delivery notice missing:\r\n"+msg_out);
-								lh_worker.write("Sending delivery request");
-								
-								Message msg_deliveryNoticeRequest = new Message(Type.Request, Value.DeliveryNoticeRequest);
-								msg_deliveryNoticeRequest.addItem("message", msg_out.toString());
-								
-								mx_executor.send(msg_deliveryNoticeRequest);
-							}
+							lh_worker.write("Removing old sent message:\r\n"+msg_out);
+							msg_removeQueue.add(msg_out);
 						}
-					} else {
-						lh_worker.write("Removing sent message with no time sent:\r\n"+msg_out);
-						msg_removeQueue.add(msg_out);
+					} 
+					else 
+					{
+						DateTime dt_timeSent = msg_out.getTimeSent();
+
+						if(dt_timeSent.plusMinutes(1).isBeforeNow())
+						{
+							lh_worker.write("Message delivery notice missing:\r\n"+msg_out);
+							lh_worker.write("Sending delivery request");
+
+							Message msg_deliveryNoticeRequest = new Message(Type.Request, Value.DeliveryNoticeRequest);
+							msg_deliveryNoticeRequest.addItem("message", msg_out.toString());
+
+							mx_executor.send(msg_deliveryNoticeRequest);
+						}
 					}
+				} else {
+					lh_worker.write("Removing sent message with no time sent:\r\n"+msg_out);
+					msg_removeQueue.add(msg_out);
 				}
-				
-				if(msg_removeQueue.size() > 0)	
-				{
-					msg_outbox.removeAll(msg_removeQueue);
-					msg_removeQueue.clear();
-				}
-				
-				try{
-					Thread.sleep(60000); // 60 Seconds
-				} catch (Exception e) {}
+			}
+
+			// Items to be removed are added to the remove queue
+			// This ensures that the looping is not interrupted by removing a message while parsing all messages in the outbox array
+			// If the remove queue has items we may now remove them
+			if(msg_removeQueue.size() > 0)	
+			{
+				msg_outbox.removeAll(msg_removeQueue);
+				msg_removeQueue.clear();
+			}
+
+			// Wait 1 minute before checking again
+			try{
+				Thread.sleep(60000);
 			} catch (Exception e) {
-				e.printStackTrace();
+				lh_worker.write(Entry.Type.Error, "Failed to sleep MessageEnsurer thread: "+e.toString()+"\r\nNow stopping MessageEnsurer");
+				b_run = false;				
 			}
 		}
 	}
